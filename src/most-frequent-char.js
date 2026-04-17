34c68e02422b03fe62ec13201937d54bb9ff94a1
Alex Curran <alexcurran.va@gmail.com>
2026-04-17 11:45:49 -0500

    Working solution for most-frequent-char.js

---
+++ src/most-frequent-char.js
@@ -0,0 +1,27 @@
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ *
+ * @param {string} str
+ * @returns {string} - char with highest frequency in string (keep earlier one if tied count)
+ */
+function mostFrequentChar(str) {
+    let charCounts = new Map();
+    let mostFreq = str[0];
+
+    for (let char of str) {
+        const charCountPrev = charCounts.get(char) || 0;
+        charCounts.set(char, charCountPrev + 1);
+    }
+
+    for (let [char, charCount] of charCounts) {
+        if (charCount > charCounts.get(mostFreq)) {
+            mostFreq = char;
+        }
+    }
+
+    return mostFreq;
+}
+
+console.log(mostFrequentChar("potato")); // "o"
